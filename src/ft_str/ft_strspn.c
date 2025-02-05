c090a1a34624cf2db5dcb6a38ba3134db0afc5aa
Guillaume Philippe <157108514+LaGuibole@users.noreply.github.com>
2025-02-05 13:51:04 +0100

    + | ft_strspn added

---
+++ src/ft_str/ft_strspn.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strspn.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: guphilip <guphilip@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/05 13:44:54 by guphilip          #+#    #+#             */
+/*   Updated: 2025/02/05 13:47:14 by guphilip         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strspn(const char *str, const char *accept)
+{
+	size_t	i;
+	size_t	j;
+	int found;
+
+	i = 0;
+	while (str[i])
+	{
+		j = 0;
+		found = 0;
+		while (accept[j])
+		{
+			if (str[i] == accept[j])
+				found = 1;
+			j++;
+		}
+		if (!found)
+			return (i);
+		i++;
+	}
+	return (1);
+}
