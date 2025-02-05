0664d20c0451c3c5eeb548c2c5d408e844666d02
Guillaume Philippe <157108514+LaGuibole@users.noreply.github.com>
2025-02-05 13:18:34 +0100

    Update libft.h

--- includes/libft.h
+++ includes/libft.h
@@ -100,7 +100,7 @@ char	*ft_strtrim(char const *s1, char const *set);
 
 char	**ft_split(char const *s, char c);
 
-int	ft_strcmp(char *s1, char *s2);
+int		ft_strcmp(char *s1, char *s2);
 
 char	*ft_itoa(int n);
 
