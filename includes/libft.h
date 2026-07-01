1c35cc9942d46ee253a22ebc148bb5f406475c00
Guillaume Philippe <guphilip@c2r7p2.42lehavre.fr>
2026-07-01 16:19:45 +0200

    fix: adding ft_realloc declaration in header file

--- includes/libft.h
+++ includes/libft.h
@@ -67,6 +67,8 @@ void	*ft_memcpy(void *dest, const void *src, size_t n);
 
 void	*ft_memmove(void *dest, const void *src, size_t n);
 
+void	*ft_realloc(void *ptr, size_t old_size, size_t new_size);
+
 size_t	ft_strlcpy(char *dest, const char *src, size_t size);
 
 size_t	ft_strlcat(char *dest, const char *src, size_t size);
